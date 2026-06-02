ebfc3b308a8e77e35aeab5a89c06db31d3eb3d43
clarasamantha580-glitch <claqrasamantha580@gmail.com>
2026-06-02 10:35:08 +0300

    sessions and cookies

--- first_php_work/index.php
+++ first_php_work/index.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+<?php
+$cookie_name = "username";
+   $cookie_value = "Samantha Clara<br>";
+   
+   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
+?>
+<?php
+  session_start();
+  ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,6 +19,10 @@
     <form method="post">
         <label for="username">Username:</label>
         <input type="text" placeholder="username" name="username">
+        <button type="Submit">Submit</button>
+        <br>
+        <label for ="age">Age</label>
+        <input type="text" placeholder="age" name="age">
         <button type="Submit">Submit</button>
     </form>
     <?php
@@ -25,14 +38,37 @@
 
     echo $username;
 
-    
-    
-    
-    
-    //Declaring variables
-   // $name ="Samantha"
-    //$age=19
+//Control Statements
+$age=19;
 
+if($age>=18){
+  echo "Age is" . $age.".<br>That is an adult.";
+}else
+   echo "That is a minor";
+
+  // $age = $_POST["age"];
+   // echo $age;
+//<?php
+ 
+  if (isset($_COOKIE[$cookie_name])) {
+    echo "Welcome, " . $_COOKIE["username"];
+} else {
+    echo "Cookie is not set";
+}
+
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "This is my first session.";
+//$students = ["Samantha", "Lyne", "Kalerwa", "Geofry", "Lucy", "Wangari", "Dalmas"];
+
+//$number = 1;
+
+//foreach ($students as $student) {
+   // echo $number . ". " . $student . "<br>";
+  //  $number++;b
+//}
+
+?>
     ?>
 </body>
 </html>
